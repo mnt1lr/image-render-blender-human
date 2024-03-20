c7db66f591d545d6021ef7428441a823a4b10271
Vinayaka B Pattanashetti RBEI-ERD1 <pattanashetti.vinayakabasavaraj@in.bosch.com>
2024-03-20 15:57:09 +0530

    refactoring

--- src/anyhuman2/labelling/cls_label_skeleton.py
+++ src/anyhuman2/labelling/cls_label_skeleton.py
@@ -105,21 +105,25 @@ class BoneLabel:
     # enddef
     # ******************************************** END HAND LABELS ****************************************************
 
-    # ************************************* BEGIN IMPORT FUNCTIONS ****************************************************
+    # ************************************* IMPORT EYEBROWS ****************************************************
 
     def UpdateEyebrowLabels(self, _sEyebrowStyle: str, _labelFile: str):
         try:
             with open(_labelFile, "r") as jsonFile:
                 dictEyebrow = json.load(jsonFile)
             self.CreateVertexGroups(
-                _objMesh=self.objHGBody, _lLabelVertices=dictEyebrow["lLabelVertices"], _replaceExisting=True
+                _objMesh=self.objHGBody, _lLabelVertices=dictEyebrow["lLabelVertices"], _bReplaceExisting=True
             )
         except FileNotFoundError as e:
             print(f"{e}")
 
     # enddef
+    # ************************************* IMPORT EYEBROWS ****************************************************
 
-    def ImportSkeletonData(self, _sSkeletonDataFile, _replaceVertexGroups):
+    # ************************************* BEGIN IMPORT FUNCTIONS ****************************************************
+
+    # NOTE: _replaceVertexGroups (True by default), replaces existing vertex groups, skips if False
+    def ImportSkeletonData(self, _sSkeletonDataFile: str, _bReplaceVertexGroups: bool = True):
         # STEP 1: Parse input json and extract skeletal bones with constraints and vertex groups
         dicSkeleton = self.ParseSkeleton(_sInSkeletonFile=_sSkeletonDataFile)
         # STEP 2: Add vertex groups to objHGBody (Mesh)
@@ -128,9 +132,7 @@ class BoneLabel:
         else:
             lLabelVertices = []
         self.CreateVertexGroups(
-            _objMesh=self.objHGBody,
-            _lLabelVertices=lLabelVertices,
-            _replaceExisting=_replaceVertexGroups,
+            _objMesh=self.objHGBody, _lLabelVertices=lLabelVertices, _bReplaceExisting=_bReplaceVertexGroups
         )
         # STEP 3: Add bones
         self.ImportSkeletonBones(_dicSkeleton=dicSkeleton, _objArmature=self.objArmature, _objRig=self.objRig)
@@ -140,20 +142,16 @@ class BoneLabel:
         return {"FINISHED"}
 
     # add bone based on contents of _dicBone
-    def AddBone(self, _sSkeletonType, _dicBone, _objArmature, _objRig):
-        bpy.context.view_layer.objects.active = _objRig
-        bpy.ops.object.mode_set(mode="EDIT")
-
+    def AddBone(self, _sSkeletonType: str, _dicBone: dict, _objArmature: bpy.types.Armature, _objRig: bpy.types.Object):
         sNewBoneName = "AT.Label;" + _sSkeletonType + ";" + _dicBone["sName"]
         objNewBone = _objArmature.edit_bones.get(str(sNewBoneName))
         if objNewBone is not None:
             print("Error: %s already present - only updating envelope_distance & head_radius", str(sNewBoneName))
-            # _objArmature.edit_bones.remove(objNewBone)
             objNewBone.envelope_distance = _dicBone["fEnvelope"]
             objNewBone.head_radius = _dicBone["fHeadRadius"]
             return
-        objNewBone = _objArmature.edit_bones.new(sNewBoneName)
 
+        objNewBone = _objArmature.edit_bones.new(sNewBoneName)
         xParentBone = _objArmature.edit_bones.get(_dicBone["sParent"])
         objNewBone.parent = xParentBone if xParentBone else None
 
@@ -163,86 +161,81 @@ class BoneLabel:
         objNewBone.tail = lTail
         objNewBone.envelope_distance = _dicBone["fEnvelope"]
         objNewBone.head_radius = _dicBone["fHeadRadius"]
-        # bpy.ops.object.mode_set(mode="POSE")
+        print(f"Added bone_label {objNewBone.name}")
 
     # enddef
 
-    def AddConstraints(self, _dicSkeleton, _objArmature, _objRig):
+    def AddConstraints(self, _dicSkeleton: dict, _objArmature: bpy.types.Armature, _objRig: bpy.types.Object):
         # pose mode
         bpy.context.view_layer.objects.active = _objRig
         bpy.ops.object.mode_set(mode="POSE")
-
         for dicBone in _dicSkeleton["lBones"]:
             # construct bone name
             sPoseBoneName = "AT.Label;" + dicBone["sType"] + ";" + dicBone["sName"]
-
             # get pose bone
-            objPoseBone = _objRig.pose.bones[sPoseBoneName]
-
+            objPoseBone: bpy.types.PoseBone = _objRig.pose.bones[sPoseBoneName]
             # add constraints
             # TODO: Implement switch (lConstraints) statement for faster execution
-            for objConstraint in dicBone["lConstraints"]:
-                sContraintType = objConstraint["sType"]
+            for dictConstraint in dicBone["lConstraints"]:
+                sContraintType = dictConstraint["sType"]
                 print(f"Adding constraint {sContraintType} for posebone {sPoseBoneName}")
-                if sContraintType == "STRETCH_TO":
-                    self.AddConstraintStretchTo(objPoseBone, objConstraint)
+                if sContraintType == "COPY_LOCATION":
+                    self.AddConstraintCopyLocation(objPoseBone, dictConstraint)
+                elif sContraintType == "STRETCH_TO":
+                    self.AddConstraintStretchTo(objPoseBone, dictConstraint)
                 elif sContraintType == "LIMIT_LOCATION":
-                    self.AddConstraintLimitLocation(objPoseBone, objConstraint)
+                    self.AddConstraintLimitLocation(objPoseBone, dictConstraint)
                 elif sContraintType == "CHILD_OF":
-                    self.AddConstraintChildOf(objPoseBone, objConstraint)
-                elif sContraintType == "COPY_LOCATION":
-                    self.AddConstraintCopyLocation(objPoseBone, objConstraint)
+                    self.AddConstraintChildOf(objPoseBone, dictConstraint)
                 else:
                     print(f"Error: constraint {sContraintType} not implemented yet")
             # endfor
-
-        # get into object mode
         bpy.ops.object.mode_set(mode="OBJECT")
 
     # enddef
 
     # add STRETCH_TO constraint
-    def AddConstraintStretchTo(_self, _xPoseBone, _lStretchConstraint):
+    def AddConstraintStretchTo(self, _xPoseBone: bpy.types.PoseBone, _dictConstraint: dict):
         xNewConstraint = _xPoseBone.constraints.new("STRETCH_TO")
-        xNewConstraint.target = bpy.data.objects[_lStretchConstraint["target"]]
-        xNewConstraint.subtarget = _lStretchConstraint["subtarget"]
-        xNewConstraint.keep_axis = _lStretchConstraint["sKeepAxis"]
-        xNewConstraint.volume = _lStretchConstraint["sVolume"]
-        xNewConstraint.influence = _lStretchConstraint["fInfluence"]
+        xNewConstraint.target = bpy.data.objects[_dictConstraint["target"]]
+        xNewConstraint.subtarget = _dictConstraint["subtarget"]
+        xNewConstraint.keep_axis = _dictConstraint["sKeepAxis"]
+        xNewConstraint.volume = _dictConstraint["sVolume"]
+        xNewConstraint.influence = _dictConstraint["fInfluence"]
 
     # enddef
 
     # add LIMIT_LOCATION constraint
-    def AddConstraintLimitLocation(self, _xPoseBone, _lLimitLocationConstraint):
+    def AddConstraintLimitLocation(self, _xPoseBone: bpy.types.PoseBone, _dictConstraint: dict):
         xNewConstraint = _xPoseBone.constraints.new("LIMIT_LOCATION")
         xNewConstraint.target = self.objHGBody
-        xNewConstraint.type = _lLimitLocationConstraint["sType"]
-        xNewConstraint.max_x = _lLimitLocationConstraint["fMaxX"]
-        xNewConstraint.max_y = _lLimitLocationConstraint["fMaxY"]
-        xNewConstraint.max_z = _lLimitLocationConstraint["fMaxZ"]
-        xNewConstraint.min_x = _lLimitLocationConstraint["fMinX"]
-        xNewConstraint.min_y = _lLimitLocationConstraint["fMinY"]
-        xNewConstraint.min_z = _lLimitLocationConstraint["fMinZ"]
-        xNewConstraint.use_transform_limit = _lLimitLocationConstraint["bUseTransformLimit"]
-        xNewConstraint.owner_space = _lLimitLocationConstraint["sOwnerSpace"]
-        xNewConstraint.influence = _lLimitLocationConstraint["fInfluence"]
+        xNewConstraint.type = _dictConstraint["sType"]
+        xNewConstraint.max_x = _dictConstraint["fMaxX"]
+        xNewConstraint.max_y = _dictConstraint["fMaxY"]
+        xNewConstraint.max_z = _dictConstraint["fMaxZ"]
+        xNewConstraint.min_x = _dictConstraint["fMinX"]
+        xNewConstraint.min_y = _dictConstraint["fMinY"]
+        xNewConstraint.min_z = _dictConstraint["fMinZ"]
+        xNewConstraint.use_transform_limit = _dictConstraint["bUseTransformLimit"]
+        xNewConstraint.owner_space = _dictConstraint["sOwnerSpace"]
+        xNewConstraint.influence = _dictConstraint["fInfluence"]
 
     # enddef
 
     # add CHILD_OF constraint
-    def AddConstraintChildOf(self, _xPoseBone, lChildOfConstraint):
+    def AddConstraintChildOf(self, _xPoseBone: bpy.types.PoseBone, _dictConstraint: dict):
         xNewConstraint = _xPoseBone.constraints.new("CHILD_OF")
         xNewConstraint.target = self.objHGBody
-        xNewConstraint.subtarget = lChildOfConstraint["sSubtarget"]
-        xNewConstraint.keep_axis = lChildOfConstraint["sKeepAxis"]
-        xNewConstraint.volume = lChildOfConstraint["sVolume"]
-        xNewConstraint.influence = lChildOfConstraint["fInfluence"]
+        xNewConstraint.subtarget = _dictConstraint["sSubtarget"]
+        xNewConstraint.keep_axis = _dictConstraint["sKeepAxis"]
+        xNewConstraint.volume = _dictConstraint["sVolume"]
+        xNewConstraint.influence = _dictConstraint["fInfluence"]
 
     # enddef
 
     # NOTE: Only supported targets = HG_Body & HG_Eyes
-    def AddTarget(self, _lConstraintCopyLocation):
-        sTarget = _lConstraintCopyLocation["sTarget"]
+    def AddTarget(self, _dictConstraintCopyLocation: dict):
+        sTarget = _dictConstraintCopyLocation["sTarget"]
         if sTarget.startswith("HG_Body"):
             return self.objHGBody
         elif sTarget.startswith("HG_Eyes"):
@@ -254,35 +247,38 @@ class BoneLabel:
     # enddef
 
     # add COPY_LOCATION constraint
-    def AddConstraintCopyLocation(self, _xPoseBone, _lConstraintCopyLocation):
+    def AddConstraintCopyLocation(self, _xPoseBone: bpy.types.PoseBone, _dictConstraint: dict):
         xNewConstraint = _xPoseBone.constraints.new("COPY_LOCATION")
-        xNewConstraint.target = self.AddTarget(_lConstraintCopyLocation)
-        xNewConstraint.subtarget = _lConstraintCopyLocation["sSubtarget"]
-        xNewConstraint.use_x = _lConstraintCopyLocation["bUseX"]
-        xNewConstraint.use_y = _lConstraintCopyLocation["bUseY"]
-        xNewConstraint.use_z = _lConstraintCopyLocation["bUseZ"]
-        xNewConstraint.invert_x = _lConstraintCopyLocation["bInvertX"]
-        xNewConstraint.invert_y = _lConstraintCopyLocation["bInvertY"]
-        xNewConstraint.invert_z = _lConstraintCopyLocation["bInvertZ"]
-        xNewConstraint.target_space = _lConstraintCopyLocation["sTargetSpace"]
-        xNewConstraint.owner_space = _lConstraintCopyLocation["sOwnerSpace"]
-        xNewConstraint.influence = _lConstraintCopyLocation["fInfluence"]
+        xNewConstraint.target = self.AddTarget(_dictConstraint)
+        xNewConstraint.subtarget = _dictConstraint["sSubtarget"]
+        xNewConstraint.use_x = _dictConstraint["bUseX"]
+        xNewConstraint.use_y = _dictConstraint["bUseY"]
+        xNewConstraint.use_z = _dictConstraint["bUseZ"]
+        xNewConstraint.invert_x = _dictConstraint["bInvertX"]
+        xNewConstraint.invert_y = _dictConstraint["bInvertY"]
+        xNewConstraint.invert_z = _dictConstraint["bInvertZ"]
+        xNewConstraint.target_space = _dictConstraint["sTargetSpace"]
+        xNewConstraint.owner_space = _dictConstraint["sOwnerSpace"]
+        xNewConstraint.influence = _dictConstraint["fInfluence"]
 
     # enddef
 
     # Import bones and add to rig
-    def ImportSkeletonBones(self, _dicSkeleton, _objArmature, _objRig):
+    def ImportSkeletonBones(self, _dicSkeleton: dict, _objArmature: bpy.types.Armature, _objRig: bpy.types.Object):
         try:
             sSkeletonType = _dicSkeleton["sSkeletonType"]
+            bpy.context.view_layer.objects.active = _objRig
+            bpy.ops.object.mode_set(mode="EDIT")
             for dicBone in _dicSkeleton["lBones"]:
                 self.AddBone(sSkeletonType, dicBone, _objArmature, _objRig)
+            bpy.ops.object.mode_set(mode="OBJECT")
         except ValueError as e:
             print(f"ERROR: {e}")
 
     # enddef
 
     # import skeleton from json file
-    def ParseSkeleton(self, _sInSkeletonFile):
+    def ParseSkeleton(self, _sInSkeletonFile: str):
         try:
             with open(_sInSkeletonFile, "r") as sJsonFile:
                 dicSkeleton = json.load(sJsonFile)
@@ -293,7 +289,8 @@ class BoneLabel:
 
     # enddef
 
-    def AddVertexGroupToMesh(self, _objMesh, _dicVertexGroup: dict, _replaceExisting=True):
+    # NOTE: _replaceExisting (True by default), replaces existing vertex groups, skips if False
+    def AddVertexGroupToMesh(self, _objMesh: bpy.types.Object, _dicVertexGroup: dict, _bReplace=True):
         try:
             for dictLabel in _dicVertexGroup["lLabels"]:
                 sVertexGroup = dictLabel["sName"]
@@ -303,13 +300,13 @@ class BoneLabel:
                     print(f"Creating vertex group {sVertexGroup} in {_objMesh.name}")
                     xVertexGroup = _objMesh.vertex_groups.new(name=sVertexGroup)
                     xVertexGroup.add(lVertices, 1.0, "ADD")
-                elif _replaceExisting:
-                    print(f"Vertex Group {sVertexGroup} already exists in {_objMesh.name}, replacing as specified")
+                elif _bReplace:
+                    print(f"Vertex Group {sVertexGroup} already exists in {_objMesh.name}, replacing..")
                     _objMesh.vertex_groups.remove(_objMesh.vertex_groups[iIndex])
                     xVertexGroup = _objMesh.vertex_groups.new(name=sVertexGroup)
                     xVertexGroup.add(lVertices, 1.0, "ADD")
                 else:
-                    print(f"Vertex Group {sVertexGroup} already exists in {_objMesh.name}")
+                    print(f"Vertex Group {sVertexGroup} already exists in {_objMesh.name}, skipping..")
                     pass
         except ValueError as e:
             print(f"ERROR: {e}")
@@ -317,17 +314,17 @@ class BoneLabel:
     # enddef
 
     # import and create vertex groups
-    def CreateVertexGroups(self, _objMesh, _lLabelVertices, _replaceExisting=True):
+    def CreateVertexGroups(self, _objMesh: bpy.types.Object, _lLabelVertices: list, _bReplaceExisting: bool):
         for dicVertexGroup in _lLabelVertices:
             sObject = dicVertexGroup["sObject"]
             try:
                 if sObject.startswith("HG_Body"):
                     self.AddVertexGroupToMesh(
-                        _objMesh=self.objHGBody, _dicVertexGroup=dicVertexGroup, _replaceExisting=True
+                        _objMesh=self.objHGBody, _dicVertexGroup=dicVertexGroup, _bReplace=_bReplaceExisting
                     )
                 elif sObject.startswith("HG_Eyes"):
                     self.AddVertexGroupToMesh(
-                        _objMesh=self.objEyes, _dicVertexGroup=dicVertexGroup, _replaceExisting=True
+                        _objMesh=self.objEyes, _dicVertexGroup=dicVertexGroup, _bReplace=_bReplaceExisting
                     )
                 else:
                     print(f"Logic not implemented for Mesh/Object: {sObject}")
